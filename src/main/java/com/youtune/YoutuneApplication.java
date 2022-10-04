651eb0b7e539fa4a7eed81aa2e8a66b22be570aa
Mateo Esquivel <mateo.esro1@gmail.com>
2022-10-03 23:24:14 -0600

    Tarea 2

---
+++ src/main/java/com/youtune/YoutuneApplication.java
@@ -0,0 +1,13 @@
+package com.youtune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutuneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youtune/YoutuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
